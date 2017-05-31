7000007dfde77dd9fbe6e2e27836cc402d0ca526
Justin Veyna <jcveyna@uci.edu>
2017-05-31 12:53:59 -0700

    Initial results
    0.9154

---
+++ main.py
@@ -0,0 +1,35 @@
+'''
+Created on May 31, 2017
+
+@author: Shintomo
+'''
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+if __name__ == '__main__':
+    mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+    x = tf.placeholder(tf.float32, [None,784])
+    W = tf.Variable(tf.zeros([784,10]))
+    b = tf.Variable(tf.zeros([10]))
+    y = tf.nn.softmax(tf.matmul(x, W) + b)
+    
+    y_= tf.placeholder(tf.float32, [None, 10])
+    cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+    #tf.nn.softmax_cross_entropy_with_logits, logits = y , labels = y_
+    train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+    
+    #run session
+    sess = tf.InteractiveSession()
+    #initialize vars
+    tf.global_variables_initializer().run()
+    
+    #run 1000 steps
+    for _ in range(1000):
+        #stochastic, batch gradient descent
+        batch_xs, batch_ys = mnist.train.next_batch(100)
+        sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+    
+    #calculate accuracy    
+    correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+
